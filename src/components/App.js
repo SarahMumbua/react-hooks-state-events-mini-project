7c4e6a468295a920760a6009368cce0a12987e9b
SarahMumbua <mumbuasarah2030@gmail.com>
2023-04-21 11:38:21 +0300

    completed the assignment

--- src/components/App.js
+++ src/components/App.js
@@ -1,21 +1,39 @@
-import React from "react";
+import React, { useState } from "react";
 import CategoryFilter from "./CategoryFilter";
 import NewTaskForm from "./NewTaskForm";
 import TaskList from "./TaskList";
 
 import { CATEGORIES, TASKS } from "../data";
-console.log("Here's the data you're working with");
-console.log({ CATEGORIES, TASKS });
 
 function App() {
-  return (
-    <div className="App">
-      <h2>My tasks</h2>
-      <CategoryFilter />
-      <NewTaskForm />
-      <TaskList />
-    </div>
-  );
+const [tasks, setTasks] = useState(TASKS);
+const [selectedCategory, setSelectedCategory] = useState("All");
+
+const handleCategorySelect = (category) => {
+setSelectedCategory(category);
+};
+
+const handleTaskFormSubmit = (newTask) => {
+setTasks([...tasks, newTask]);
+};
+
+let visibleTasks = tasks;
+if (selectedCategory !== "All") {
+visibleTasks = tasks.filter((task) => task.category === selectedCategory);
+}
+
+return (
+<div className="App">
+<h2>My tasks</h2>
+<CategoryFilter
+     categories={CATEGORIES}
+     selectedCategory={selectedCategory}
+     onSelectCategory={handleCategorySelect}
+   />
+<NewTaskForm categories={CATEGORIES} onTaskFormSubmit={handleTaskFormSubmit} />
+<TaskList tasks={visibleTasks} />
+</div>
+);
 }
 
 export default App;
